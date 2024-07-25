72f9795b03015a4150edab84f67de58cc88cb26e
Abdullah-Talaat <abdullah.talaat123.js@gmail.com>
2024-07-25 18:33:23 +0300

    Add files via upload

--- senah.js
+++ senah.js
@@ -1,15 +1,13 @@
-
-let senahMood = false;
 let senah = document.querySelector('.senah')
 function senahFunction() {
   if (!senahMood) {
     senah.style.display = "none";
   }
   else{
-  senah.style.display = "flex";
+    senah.style.display = "flex";
   }
 }
  
 
-senahMood = false;
+senahMood = true;
 senahFunction();
